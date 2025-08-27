e327d651a19ae2d14cc5dda4fff0a620d9f60352
Christian <Christian@DESKTOP-LR4DPNS>
2025-08-27 16:03:23 -0300

    make more smooth

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-particlesJS("particles-js", {
-    particles: {
-        number: { value: 120, density: { enable: true, value_area: 800 } },
+particlesJS("particles-js", 
+    {
+    particles: 
+    {
+        number: { value: 200, density: { enable: true, value_area: 700 } },
         color: { value: ["#ff0000","#00bfff","#ff69b4","#ffa500","#ffff00"] },
         shape: { type: "circle" },
-        opacity: { value: 2, random: true },
-        size: { value: 3, random: true, anim: { enable: true, speed: 3, size_min: 1, sync: false } },
-        line_linked: { enable: true, distance: 90, color: "#444444", opacity: 1, width: 1 },
-        move: { enable: true, speed: 1.2, random: true, straight: false, out_mode: "out" }
+        opacity: { value: 5, random: true },
+        size: { value: 3, random: true, anim: { enable: true, speed: 1, size_min: 1, sync: false } },
+        line_linked: { enable: true, distance: 70, color: "#444444", opacity: 1, width: 1 },
+        move: { enable: true, speed: 0.9, random: true, straight: false, out_mode: "out" }
     },
-    interactivity: {
+    interactivity: 
+    {
         detect_on: "canvas",
-        events: {
+        events: 
+        {
             onhover: { enable: true, mode: ["grab","repulse"] },
             onclick: { enable: true, mode: "push" }
         },
-        modes: {
-            grab: { distance: 200, line_linked: { opacity: 0.5 } },
-            repulse: { distance: 120, duration: 0.4 },
-            push: { particles_nb: 4 }
+        modes: 
+        {
+            grab: { distance: 100, line_linked: { opacity: 0.8 } },
+            repulse: { distance: 100, duration: 0.1 },
+            push: { particles_nb: 6}
         }
     },
     retina_detect: true
